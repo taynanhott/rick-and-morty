ce9fa941bb532a8d66a38aa84b6cddb4960ded7a
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-12-19 15:33:06 -0300

    code polishing

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,7 +6,7 @@ export default function Search({handleSearch, setSearch, search}) {
         >
             <input
                 type="text"
-                placeholder="Search for a character..."
+                placeholder="Search name..."
                 className="px-4 py-2 rounded-l-lg focus:outline-none text-emerald-400 w-40 md:w-56 lg:w-64"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
