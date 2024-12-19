0ef1f61576e8c5ae1351adb4e7e1e1cde3db5f9b
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-12-19 15:29:28 -0300

    fixed bug's in layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
-import RadioGroup from "./components/radio";
-import Loading from "./components/loading";
-import { motion } from "framer-motion";
-import Card from "./components/card";
+
 import './index.css'
+import Card from "./components/card";
 import ToTop from "./components/toTop";
+import { motion } from "framer-motion";
+import Search from "./components/search";
+import Loading from "./components/loading";
+import RadioGroup from "./components/radio";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -12,12 +14,44 @@ export default function App() {
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("");
-  const [info, setInfo] = useState([]);
+  const [count, setCount] = useState(0);
   const [page, setPage] = useState(1);
+
+  // --------------------------------------------------------------------------------------------------
+
+  useEffect(() => {
+    if (page === 1 && characters.length === 0) {
+      fetchCharacters(page, search);
+    }
+  }, [page, search, characters.length]);
+
+  useEffect(() => {
+    setPage(1);
+    setCharacters([]);
+    fetchCharacters(1, search);
+  }, [filter]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [page, search]);
+
+  // --------------------------------------------------------------------------------------------------
 
   const fetchCharacters = async (currentPage, query = "") => {
     if (loading) return;
     setLoading(true);
+    setCount(0);
     try {
 
       const filterApi = filter !== 'all' ? `&status=${filter}` : "";
@@ -28,7 +62,7 @@ export default function App() {
       const data = await response.json();
 
       if (!data.error) {
-        setInfo(data.info);
+        setCount(data.info.count);
         setCharacters((prevCharacters) => {
           const newCharacters = [...prevCharacters, ...data.results];
           const uniqueCharacters = newCharacters.filter(
@@ -48,17 +82,7 @@ export default function App() {
     }
   };
 
-  useEffect(() => {
-    if (page === 1 && characters.length === 0) {
-      fetchCharacters(page, search);
-    }
-  }, [page, search, characters.length]);
-
-  useEffect(() => {
-    setPage(1);
-    setCharacters([]);
-    fetchCharacters(1, search);
-  }, [filter]);
+  // --------------------------------------------------------------------------------------------------
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -66,13 +90,6 @@ export default function App() {
     setCharacters([]);
     fetchCharacters(1, search);
   };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   const handleScroll = () => {
     if (
@@ -86,14 +103,6 @@ export default function App() {
     setIsVisible(window.scrollY > 300);
   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [page, search]);
-
   return (
     <div className="min-h-screen w-full text-white relative px-14 pt-6 pb-14 bg-gradient-to-r from-gray-900 via-emerald-900 to-gray-900 animated-gradient">
       <img
@@ -102,29 +111,11 @@ export default function App() {
         className="mx-auto w-72 mb-6"
       />
 
-      <form
-        className="flex justify-center items-center"
-        onSubmit={handleSearch}
-      >
-        <input
-          type="text"
-          placeholder="Search for a character..."
-          className="px-4 py-2 rounded-l-lg focus:outline-none text-emerald-400 w-40 md:w-56 lg:w-64"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-
-        <button
-          type="submit"
-          className="bg-emerald-500 px-4 py-2 rounded-r-lg hover:bg-emerald-700 text-white font-semibold"
-        >
-          Search
-        </button>
-      </form>
+      <Search handleSearch={handleSearch} setSearch={setSearch} search={search} />
 
       <RadioGroup setFilter={setFilter} className="flex justify-center items-center gap-6 mt-2" />
 
-      <p className="text-center my-4">Results: {!loading && info.count ? info.count : 0}</p>
+      <p className="text-center my-4">Results: {!loading ? count : 0}</p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {characters.map((character, index) => (
---
+++ src/components/search.jsx
@@ -0,0 +1,23 @@
+export default function Search({handleSearch, setSearch, search}) {
+    return (
+        <form
+            className="flex justify-center items-center"
+            onSubmit={handleSearch}
+        >
+            <input
+                type="text"
+                placeholder="Search for a character..."
+                className="px-4 py-2 rounded-l-lg focus:outline-none text-emerald-400 w-40 md:w-56 lg:w-64"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+
+            <button
+                type="submit"
+                className="bg-emerald-500 px-4 py-2 rounded-r-lg hover:bg-emerald-700 text-white font-semibold"
+            >
+                Search
+            </button>
+        </form>
+    );
+}
